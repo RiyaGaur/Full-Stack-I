546e48c44b3e5109682ed0046aca5f2ef7da6124
RiyaGaur <riyagaur15@gmail.comm>
2020-11-20 17:34:03 +0530

    Form Validation

---
+++ Form Validation/form.js
@@ -0,0 +1,72 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const small = document.querySelector('small');
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    checkInput();
+})
+
+function checkInput() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const password1Value = password1.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        showError(username, "Username can not be Blank");
+    }
+    else {
+        showSuccess(username);
+    }
+
+
+    if (emailValue === '') {
+        showError(email, "Email can not be Blank");
+    }
+    else if (!isEmailValid(emailValue)) {
+        showError(email, "Email Id is not valid")
+    }
+    else {
+        showSuccess(email);
+    }
+
+
+    if (password1Value === '') {
+        showError(password1, "Password can not be Blank");
+    }
+    else {
+        showSuccess(password1);
+    }
+
+    if (password2Value === '') {
+        showError(password2, "Password can not be Blank");
+    }
+    else if (password2Value != password1Value) {
+        showError(password2, "Passwords not match")
+    }
+    else {
+        showSuccess(password2);
+    }
+}
+
+function showError(input, msg) {
+    const formControl = input.parentNode;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerHTML = msg;
+    console.log(small);
+}
+
+function showSuccess(input) {
+    const formControl = input.parentNode;
+    formControl.className = 'form-control success';
+}
+
+function isEmailValid(email) {
+    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(email);
+}
